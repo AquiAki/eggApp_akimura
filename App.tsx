a5f8d422101ef4264019fba8120d8bf9c8ed64ed
AquiAki <theorange.01@gmail.com>
2020-11-25 16:20:38 +0900

    eggApp完成

--- App.tsx
+++ App.tsx
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { render } from "react-dom";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+import egg1 from "./assets/egg01.png";
+import egg2 from "./assets/egg02.png";
+import egg3 from "./assets/egg03.png";
 
 export default function App() {
+  const [count, setCount] = React.useState(100);
+  const [message, setMessage] = React.useState("100回おせ");
+  const [img, setImg] = React.useState(egg1);
+
+  const numChange = () => {
+    setCount(count - 1);
+    if (count === 76) {
+      setMessage("まだまだ");
+    }
+
+    if (count === 51) {
+      setMessage("頑張れ");
+      setImg(egg2);
+    }
+    if (count === 26) {
+      setMessage("もうちょい");
+    }
+    if (count === 1) {
+      setMessage("おめでとう！");
+      setImg(egg3);
+      
+
+      setTimeout(function () {
+        preventCrush();
+      }, 5000);
+    }
+  };
+
+  const preventCrush = () => {
+    setCount(100);
+    setImg(egg1);
+    setMessage("もう一度100回おして")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.num}>{count}</Text>
+
+      <TouchableOpacity onPress={numChange}>
+        <Image style={styles.image} source={img} />
+      </TouchableOpacity>
+
+      <Text style={styles.mess}> {message}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +66,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+
+  num: {
+    fontSize: 30,
+    backgroundColor: "black",
+    color: "white",
+    width: "45%",
+    textAlign: "center",
+  },
+
+  mess: {
+    fontSize: 40,
+    color : "blue",
   },
 });
+
